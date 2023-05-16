c949c2f272566b0d721193fc1875f1246cf28f50
Martin Koller <kollermartin00@gmail.com>
2023-05-16 17:14:45 +0200

    feat: create api service and schema

---
+++ src/app/users/users-list/data/service/users.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "../schema/users";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private _http: HttpClient) {
+  }
+
+  public getUsers(): Observable<User> {
+    return this._http.get<User>('https://jsonplaceholder.typicode.com/users');
+  }
+}
