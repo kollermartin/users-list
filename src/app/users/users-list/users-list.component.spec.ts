c5770093d50f26ca0e5433c6ae527228cbb41b1b
Martin Koller <kollermartin00@gmail.com>
2023-05-16 21:38:53 +0200

    feat: Add unit tests for users list

--- src/app/users/users-list/users-list.component.spec.ts
+++ src/app/users/users-list/users-list.component.spec.ts
@@ -1,8 +1,58 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {UsersListComponent} from './users-list.component';
+import {User} from "../data/schema/users";
 
 describe('UsersListComponent', () => {
+  const usersMockData: User[] = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      address: {
+        street: 'Kulas Light',
+        suite: 'Apt. 556',
+        city: 'Gwenborough',
+        zipcode: '92998-3874',
+        geo: {
+          lat: '-37.3159',
+          lng: '81.1496',
+        },
+      },
+      phone: '1-770-736-8031 x56442',
+      website: 'hildegard.org',
+      company: {
+        name: 'Romaguera-Crona',
+        catchPhrase: 'Multi-layered client-server neural-net',
+        bs: 'harness real-time e-markets',
+      },
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      address: {
+        street: 'Victor Plains',
+        suite: 'Suite 879',
+        city: 'Wisokyburgh',
+        zipcode: '90566-7771',
+        geo: {
+          lat: '-43.9509',
+          lng: '-34.4618',
+        },
+      },
+      phone: '010-692-6593 x09125',
+      website: 'anastasia.net',
+      company: {
+        name: 'Deckow-Crist',
+        catchPhrase: 'Proactive didactic contingency',
+        bs: 'synergize scalable supply-chains',
+      },
+    },
+  ];
+
   let component: UsersListComponent;
   let fixture: ComponentFixture<UsersListComponent>;
 
@@ -14,10 +64,27 @@ describe('UsersListComponent', () => {
 
     fixture = TestBed.createComponent(UsersListComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should render users list', () => {
+    component.users = [...usersMockData];
+    fixture.detectChanges();
+
+    const userElements = fixture.nativeElement.querySelectorAll('.users-list__cell');
+    expect(userElements.length).toBe(usersMockData.length);
+  });
+
+  it('should not render users list', () => {
+    fixture.detectChanges();
+
+    const userElements = fixture.nativeElement.querySelectorAll('.users-list__cell');
+
+    expect(component.users).toBeUndefined();
+    expect(userElements.length).toBe(0);
+  })
 });
