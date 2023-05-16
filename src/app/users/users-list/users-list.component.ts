f33075e9132efb6b88ba00162663df0786490cce
Martin Koller <kollermartin00@gmail.com>
2023-05-16 19:26:48 +0200

    feat: render users in users-list

--- src/app/users/users-list/users-list.component.ts
+++ src/app/users/users-list/users-list.component.ts
@@ -1,10 +1,13 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {User} from "../data/schema/users";
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
-  styleUrls: ['./users-list.component.scss']
+  styleUrls: ['./users-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UsersListComponent {
 
+  @Input() users: User[] | undefined;
 }
